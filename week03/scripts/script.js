e408c355c96f5adf4bb0a11c07ed2b662fabc6cf
graydenrice <graydenrice@gmail.com>
2023-09-20 14:54:41 -0600

    lazy loading

---
+++ week03/scripts/script.js
@@ -0,0 +1,7 @@
+var lastMod = document.lastModified;
+
+document.getElementById('lastModified').innerHTML = 'Last Modified: ' + lastMod;
+
+var year = new Date().getFullYear();
+
+document.getElementById('year').innerHTML = year;
