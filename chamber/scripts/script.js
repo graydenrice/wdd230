2923f8c692a60f77cf0b3a03371bc30510bd10fc
graydenrice <graydenrice@gmail.com>
2023-09-21 20:52:40 -0600

    discover page

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -11,3 +11,24 @@ document.getElementById('lastModified').innerHTML = 'Last Modified: ' + lastMod;
 
 const year = new Date().getFullYear();
 document.getElementById('year').innerHTML = year;
+
+const firstVisit = localStorage.getItem('lastVisit');
+const today = Date.now();
+const visitElement = document.getElementById('visits');
+
+if (!firstVisit) {
+    localStorage.setItem('lastVisit', today);
+    visitElement.textContent = 'Welcome, Let us know if you need anything';
+} else {
+    const sinceLast = today - firstVisit;
+    const daysSince = Math.floor(sinceLast / (1000 * 60 * 60 * 24));
+
+    if (daysSince < 1) {
+        visitElement.textContent = 'Back so soon, Awesome!';
+    } else {
+        const word = (daysSince === 1) ? 'day' : 'days';
+        visitElement.textContent = `It has been ${daysSince} ${word} since your last visit`;
+    }
+
+    localStorage.setItem('lastVisit', today);
+}
