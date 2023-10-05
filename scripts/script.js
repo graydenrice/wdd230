f8dbc8010495347c6469bb6685a90a2ccf1a5a6d
graydenrice <graydenrice@gmail.com>
2023-10-04 18:09:14 -0600

    team activity

--- scripts/script.js
+++ scripts/script.js
@@ -39,8 +39,6 @@ const pass1 = document.getElementById('pass');
 const pass2 = document.getElementById('passw2');
 const message = document.getElementById('message')
 
-// pass2.addEventListener('focusout', checkSame);
-
 function checkSame() {
     if (pass1 !== pass2.value) {
         message.textContent = "Passwords Do Not Match!";
@@ -57,9 +55,6 @@ function checkSame() {
 
 const rvalue = document.getElementById("value");
 const rate = document.getElementById("rate");
-
-// rate.addEventListener('change', displayValue);
-// rate.addEventListener('input', displayValue);
 
 function displayValue() {
     rvalue.innerHTML = rate.value;
